c75d0bf9474bfbd1b7dffbc265a2738ee4eff97f
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-09 09:25:02 +0100

    Cosas sin importancia

--- Frontend/Trabajo/Trabajo/MainWindow.xaml.cs
+++ Frontend/Trabajo/Trabajo/MainWindow.xaml.cs
@@ -22,15 +22,69 @@ namespace Trabajo
     {
         public MainWindow()
         {
-            this.WindowState = WindowState.Maximized;
+            
             InitializeComponent();
+           
+            userBox.Text = "Usuario";
+
+            userBox.GotFocus += RemoveTextUser;
+            userBox.LostFocus += AddTextUser;
+
+
+            passBox.Text = "Contraseña";
+
+            passBox.GotFocus += RemoveTextPass;
+            passBox.LostFocus += AddTextPass;
+        }
+
+        public void RemoveTextUser(object sender, EventArgs e)
+        {
+            if (userBox.Text == "Usuario")
+            {
+                userBox.Text = "";
+            }
+        }
+
+        public void AddTextUser(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userBox.Text))
+                userBox.Text = "Usuario";
 
         }
+
+        public void RemoveTextPass(object sender, EventArgs e)
+        {
+
+            if (passBox.Text == "Contraseña")
+            {
+                passBox.Text = "";
+            }
+        }
+
+        public void AddTextPass(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(passBox.Text))
+                passBox.Text = "Contraseña";
+        }
+
+        /*private void mostrarPantallaRegistro(object sender, EventArgs e) {
+
+            Register registro = new Register();
+            registro.Show();
+        }
+        */
+
 
         //Boton de iniciar sesion
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void mostrarPantallaRegister(object sender, RoutedEventArgs e) {
+            Register registro = new Register();
+            registro.Show();
         }
 
         //Boton de registrarte
